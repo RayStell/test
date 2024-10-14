ba2b51348fbe556db66930e4be81406dc377220c
Your Name <you@example.com>
2024-10-14 09:23:49 +0700

    Создал форму регистрации

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-<?php echo "Hello world"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration Form</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="name">Имя:</label>
+        <input type="text" name="name" id="name"><br><br>
+        <label for="password">Пароль:</label>
+        <input type="password" name="password" id="password"><br><br>
+        <label for="email">Почта:</label>
+        <input type="email" name="email" id="email"><br><br>
+        <input type="submit" value="Register">
+    </form>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_POST["name"];
+        $password = $_POST["password"];
+        $email = $_POST["email"];
+        print_r(array(
+            "Name" => $name,
+            "Password" => $password,
+            "Email" => $email
+        ));
+    }
+    ?>
+</body>
+</html>
